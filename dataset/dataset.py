8d1c94626ff8acaba0a9a75960d325531367f542
Christian Dalvit <chris.dalvit@gmail.com>
2025-02-13 22:18:41 +0100

    implement simple dataset

---
+++ dataset/dataset.py
@@ -0,0 +1,32 @@
+import json
+import torch
+from torch_geometric.data import Dataset
+from torch_geometric.data import Data
+
+
+class ConvexHullDataset(Dataset):
+    
+    def __init__(self, root):
+        super().__init__(root)
+        self.root = root
+        with open(self.root) as f:
+            self.data = json.loads(f.read())
+        
+    @property
+    def dimension(self):
+        return len(self.data[0]['points'][0])    
+    
+    def len(self):
+        return len(self.data)
+
+    def get(self, idx):
+        return Data(
+            x=torch.tensor(self.data[idx]['vertices']),
+            y=torch.tensor(self.data[idx]['volume'])
+        )
+        # return { 
+        #    'points': torch.tensor(self.data[idx]['points']),
+        #    'vertices': torch.tensor(self.data[idx]['vertices']),
+        #    'volume': torch.tensor(self.data[idx]['volume']),
+        #    'area': torch.tensor(self.data[idx]['area'])
+        # }
