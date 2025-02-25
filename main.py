459ed5456f73c96ec16bd4c45c87db3f5dcfacd9
Christian Dalvit <chris.dalvit@gmail.com>
2025-02-25 16:38:56 +0100

    implement first reasonable learning pipeline

--- main.py
+++ main.py
@@ -4,26 +4,35 @@ from torch_geometric.loader import DataLoader
 from dataset.dataset import ConvexHullDataset
 from model import ConvexHullModel
 
-dataset = ConvexHullDataset(root='dataset/dataset.json')
-dataloader = DataLoader(dataset, batch_size=64)
-model = ConvexHullModel(dataset.dimension)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.2)
-loss_fn = L1Loss()
+train_dataset = ConvexHullDataset(root='dataset/train.json')
+test_dataset = ConvexHullDataset(root='dataset/test.json')
+train_loader = DataLoader(train_dataset, batch_size=8)
+test_loader = DataLoader(test_dataset, batch_size=8)
 
-for epoch in range(15):
+model = ConvexHullModel(train_dataset.dimension)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0008)
+criterion = L1Loss()
+
+def compute_eval_metric(model, dataloader, criterion):
+    model.eval()
     losses = []
     for sample in dataloader:
+        pred = model(sample)
+        loss = criterion(pred, sample.y)
+        losses.append(loss)
+    return torch.stack(losses, dim=0).mean().item()
+
+for epoch in range(300):
+    model.train()
+    losses = []
+    for sample in train_loader:
         optimizer.zero_grad()
         pred = model(sample)
-        loss = loss_fn(pred, sample.y)
+        loss = criterion(pred, sample.y)
         loss.backward()
         optimizer.step()
         losses.append(loss)
-    print(f'Loss {epoch}: {torch.stack(losses, dim=0).mean().item():.2f}')
-
-for sample in dataloader:
-    pred = model(sample)
-    print(pred)
-    print(sample.y)
-    print(sample.y.mean())
-    break
+    if epoch % 10 == 0:
+        eval_loss = compute_eval_metric(model, test_loader, criterion)
+        train_loss = torch.stack(losses, dim=0).mean().item()
+        print(f'Loss {epoch} -> Train: {train_loss:.2f} / Test: {eval_loss:.2f}')
--- model.py
+++ model.py
@@ -1,25 +1,41 @@
 import torch
 from torch.nn import Linear
-from torch_geometric.nn import GCNConv, global_add_pool
+from torch_geometric.nn import GraphConv, global_max_pool
+import torch.nn.init as init
 
 class ConvexHullModel(torch.nn.Module):
     def __init__(self, point_dim):
         super().__init__()
-        #torch.manual_seed(1234)
-        self.conv1 = GCNConv(point_dim, 2)
-        self.conv2 = GCNConv(2, 2)
-        self.conv3 = GCNConv(2, 2)
-        self.regression = Linear(2, 1)
+        hidden_dim = 32
+        self.conv1 = GraphConv(point_dim, hidden_dim)
+        self.conv2 = GraphConv(hidden_dim, hidden_dim // 2)
+        self.conv3 = GraphConv(hidden_dim // 2, hidden_dim // 4)
+        self.lin = Linear(hidden_dim // 4, 1)
+        
+    def _initialize_weights(self): # Initialization is important for training
+        init.xavier_uniform_(self.conv1.weight)
+        init.xavier_uniform_(self.conv2.weight)
+        init.xavier_uniform_(self.conv3.weight)
+        init.xavier_uniform_(self.lin.weight)
+        if self.conv1.bias is not None:
+            init.zeros_(self.conv1.bias)
+        if self.conv2.bias is not None:
+            init.zeros_(self.conv2.bias)
+        if self.conv3.bias is not None:
+            init.zeros_(self.conv3.bias)
+        if self.lin.bias is not None:
+            init.zeros_(self.lin.bias)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-        h = self.conv1(x, edge_index)
-        h = h.relu()
-        h = self.conv2(h, edge_index)
-        h = h.relu()
-        h = self.conv3(h, edge_index)
-        h = h.relu()
-
-        h = global_add_pool(h, batch)
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+        x = x.relu()
         
-        return self.regression(h).squeeze(1)
+        x = global_max_pool(x, batch)
+        x = self.lin(x)
+        
+        return x.squeeze(1)
