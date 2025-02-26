113063832db4d944469c95e001339139d065b971
Christian Dalvit <chris.dalvit@gmail.com>
2025-02-26 18:08:09 +0100

    introduce plotting

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from torch.nn import L1Loss
 from torch_geometric.loader import DataLoader
 from dataset.dataset import ConvexHullDataset
 from model import ConvexHullModel
+import matplotlib.pyplot as plt
 
 undirected = False
 normalize = True
+batch_size = 32
 train_dataset = ConvexHullDataset(root='dataset/train.json', undirected=undirected, normalize=normalize)
 test_dataset = ConvexHullDataset(root='dataset/test.json', undirected=undirected, normalize=normalize) 
-train_loader = DataLoader(train_dataset, batch_size=8)
-test_loader = DataLoader(test_dataset, batch_size=8)
+train_loader = DataLoader(train_dataset, batch_size=batch_size)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
 model = ConvexHullModel(train_dataset.dimension)
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.0008)
@@ -24,6 +26,8 @@ def compute_eval_metric(model, dataloader, criterion):
         losses.append(loss)
     return torch.stack(losses, dim=0).mean().item()
 
+train_losses = []
+eval_losses = []
 for epoch in range(250):
     model.train()
     losses = []
@@ -37,6 +41,11 @@ for epoch in range(250):
     if epoch % 10 == 0:
         eval_loss = compute_eval_metric(model, test_loader, criterion)
         train_loss = torch.stack(losses, dim=0).mean().item()
+        eval_losses.append(eval_loss)
+        train_losses.append(train_loss)
         print(f'Loss {epoch} -> Train: {train_loss:.2f} / Test: {eval_loss:.2f}')
         
 torch.save(model.state_dict(), 'model.pth')
+plt.plot(train_losses, label='Train')
+plt.plot(eval_losses, label='Eval')
+plt.show()
